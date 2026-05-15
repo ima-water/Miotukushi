aed620861cc662b44ebdf68f1be9daa15bb62c31
mfrpczgtsy-lab <mfrpczgtsy@privaterelay.appleid.com>
2026-05-15 16:47:52 +0900

    増やすてみた

---
+++ 澪標/supabase-client.js
@@ -0,0 +1,10 @@
+const SUPABASE_URL =
+'https://orostnlovaoyzvozzjeu.supabase.co';
+
+const SUPABASE_ANON =
+'sb_publishable_VaIEHsGSIKnpNlkp5V0Leg_Drfx2Vnj';
+
+const supabase = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_ANON
+);
